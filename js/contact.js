15708e41371cb639c25c2498f5099621318f3325
cameronmoodley <moodley.cameron@gmail.com>
2020-02-15 14:11:17 +0100

    updated broken code

--- js/contact.js
+++ js/contact.js
@@ -2,14 +2,17 @@ document.getElementById('submit').addEventListener('click', function(){
   const firstName = document.getElementById('firstName');
   const firstNameError = document.getElementById('firstNameError');
   let isFirstNameValid = false;
+
   const email = document.getElementById('email');
   const emailError = document.getElementById('emailError');
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   let isEmailValid = false;
+
   let telNumber = document.getElementById('telNumber');
   const telNumberError = document.getElementById('telNumberError');
   const telNumberRegex = /^(\d{7})(\d{3})?$/;
   let isTelNumberValid = false;
+
   // test firstname
   if(firstName.value === ""){
     firstName.classList.add('is-danger');
@@ -33,7 +36,6 @@ document.getElementById('submit').addEventListener('click', function(){
     isEmailValid = true;
   }
 
-  console.log(telNumberRegex.test(telNumber.value))
   // test tel number
   if(!telNumberRegex.test(telNumber.value)) {
     telNumber.classList.add('is-danger');
@@ -44,7 +46,6 @@ document.getElementById('submit').addEventListener('click', function(){
     telNumberError.style.display = "none";
     isTelNumberValid = true;
   }
-
 
   (isFirstNameValid && isEmailValid && isTelNumberValid) && alert('Submitted')
 
